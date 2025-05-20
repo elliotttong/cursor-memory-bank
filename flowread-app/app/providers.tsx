caf8a265a81463c5bdcadca60c0269dfdca2b750
elliotttong <elliott.tong3897@gmail.com>
2025-05-20 20:14:11 +0100

    removed unnessary components

--- flowread-app/app/providers.tsx
+++ flowread-app/app/providers.tsx
@@ -1,8 +1,52 @@
 "use client"
 
-import { ReactNode } from 'react'
-import { PostHogProvider } from '../components/PostHogProvider'
+import { usePathname, useSearchParams } from "next/navigation"
+import { useEffect, Suspense, ReactNode } from "react"
+import { usePostHog } from 'posthog-js/react'
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
-export function Providers({ children }: { children: ReactNode }) {
-  return <PostHogProvider>{children}</PostHogProvider>
+export function PostHogProvider({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only',
+        capture_pageview: false 
+      })
+    }
+  }, [])
+
+  return (
+    <PHProvider client={posthog}>
+      <SuspendedPostHogPageView />
+      {children}
+    </PHProvider>
+  )
+}
+
+function PostHogPageView() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const posthog = usePostHog()
+
+  useEffect(() => {
+    if (pathname && posthog) {
+      let url = window.origin + pathname
+      if (searchParams && searchParams.toString()) {
+        url = url + "?" + searchParams.toString();
+      }
+      posthog.capture('$pageview', { '$current_url': url })
+    }
+  }, [pathname, searchParams, posthog])
+
+  return null
+}
+
+function SuspendedPostHogPageView() {
+  return (
+    <Suspense fallback={null}>
+      <PostHogPageView />
+    </Suspense>
+  )
 }
